aa6fbb3b8ea3164eb36bdacd48c5248965117d0a
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2022-02-11 11:42:19 +0100

    init

--- adminTache/models.py
+++ adminTache/models.py
@@ -28,15 +28,37 @@ class Seance(models.Model):
     def __str__(self):
         return(self.matiere.titre+'\t\t\t: '+str(self.date_heure_debut)+'\nNombre d\'heures\t\t: '+str(self.duree)+'h')
    
+  
 # Une tâche est identifiée par un ID autoincrémenté et consiste en une durée (en heure) et une description (max 200 car) 
 # On associera une tâche à une matière par le biais d'une clé étrangère 
 class Tache(models.Model):
     id = models.BigAutoField(primary_key=True) #clé primaire de l'entité autoincrémentée
-    seance = models.ForeignKey(Seance, on_delete=models.CASCADE)    
-    duree = models.IntegerField(default=1) # représente le nombre d'heure à réaliser pour la tâche identifiée
     description = models.CharField(max_length=200)
-    titre = models.CharField(max_length=50,default='Faire une pause')
-    faite = models.BooleanField(default=False)
+
+    # La priorité de la tache est définie par un nombre entre 1 et 5
+    priorite = models.IntegerField(default=1)
+
+    # La date de début de la tache
+    date_debut = models.DateField(default=datetime.now(timezone(timedelta(0))))
+
+    FINI = 'FINI'
+    ENCOURS = 'EN COURS'
+    ACOMMENCER = 'A COMMENCER'
+    STATUT_CHOICES = (
+        (FINI, 'FINI'),
+        (ENCOURS, 'EN COURS'),
+        (ACOMMENCER, 'A COMMENCER')
+    )
+
+    # Le statut de la tache est défini par un choix dans la liste STATUT_CHOICES
+    statut = models.CharField(
+        max_length=2,
+        choices=STATUT_CHOICES,
+        default=ACOMMENCER,
+    )
+
+    # L'etat de l'avancement de la tache est définie par un nombre entier
+    etat_avancement = models.IntegerField(default=1)
     
     def __str__(self):
-        return(self.titre+'\t\t: '+'En cours'*(not self.faite)+'Faite'*(self.faite)+'\nDescription\t\t: '+self.description+'\nNombre d\'heures\t\t: '+str(self.duree)+'h\n'+str(self.seance))
+        return(self.description+'\nPriorité\t\t\t: '+str(self.priorite)+'\nDate de début\t\t: '+str(self.date_debut)+'\nStatut\t\t\t: '+str(self.statut)+'\nEtat d\'avancement\t: '+str(self.etat_avancement))
