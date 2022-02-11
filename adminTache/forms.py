fc5565acc8bb23d6d34fbf07cca2641b1a475799
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2022-02-11 14:03:22 +0100

    delete

--- adminTache/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from adminTache.models import *
-class RechercheTacheForm(ModelForm):
-    class Meta:
-        model = Tache
-        fields = ['seance']
-        
--- adminTache/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from adminTache import views
-urlpatterns = [
-    path('', views.index,name='index'),
-    path('list/', views.tacheListDetailView.as_view(),name='tache-list'),
-    path('<int:pk>/',views.tacheDetailView.as_view(),name='tache-detail'),
-    path('matiere/<str:matiere_titre>/',views.matiere,name='matiere'),
-    path('todo/<int:tache_a_faire>/',views.toDoList,name='todo')
-]
