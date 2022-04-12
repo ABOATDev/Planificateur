b1fabe80a3211bc3d3b5df4f201a856e61fe4b4c
ABOATDev <30906528+ABOATDev@users.noreply.github.com>
2022-04-12 14:53:02 +0200

    Liste des taches

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from app import views
 urlpatterns = [
     path('', views.index,name='index'),
-    path('list/', views.tacheListDetailView.as_view(),name='tache-list'),
+    path('tache', views.tacheListDetailView.as_view(),name='tache-list'),
+    path('tache/', views.tacheListDetailView.as_view(),name='tache-list'),
+    path('tache/<int:pk>/',views.tacheDetailView.as_view(),name='tache-detail'),
 ]
