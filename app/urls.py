a7dd105441cd8e2eb0b9318c7fb2748a531ae6dc
reda <reda.guirrine1@campus-igs-toulouse.fr>
2022-04-12 14:52:51 +0200

    ADD PERSONNE DETAIL AND LIST
    Signed-off-by: reda <reda.guirrine1@campus-igs-toulouse.fr>

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 urlpatterns = [
     path('', views.index,name='index'),
     path('list/', views.tacheListDetailView.as_view(),name='tache-list'),
+    path('personne/', views.personneListDetailView.as_view(),name='personne-list'),
+    path('personne/<int:pk>/', views.personneDetailView.as_view(),name='personne-detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -10,7 +10,8 @@ from django.utils import timezone
 # Create your views here.
 def index(request):
     formulaire='<form action="" method="post"><input type="submit" value="Rechercher"></form>'
-    temp=Template(formulaire+"<br><a href='list'>Afficher la liste des tâches</a>")
+    temp=Template(formulaire+"<br><a href='list'>Afficher la liste des tâches</a>"
+    +"<br><a href='personne'>Afficher la liste des personnes</a>")
     context =RequestContext(request)
     return HttpResponse(temp.render(context))  
 
@@ -22,4 +23,18 @@ class tacheListDetailView(generic.list.ListView):
         context = super().get_context_data(**kwargs)
         for object in self.object_list:
             object.faite = object.status
+        return context
+
+class personneListDetailView(generic.list.ListView):
+    model=Personne
+    template_name='personne_list.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class personneDetailView(generic.detail.DetailView):
+    model=Personne
+    template_name='personne_detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         return context
